d56590859eafec00d00f57711c0765d4aaaca86d
i4lock <115173085+i4lock@users.noreply.github.com>
2022-11-17 17:58:31 +0100

    lets go enfin

---
+++ rick.sh
@@ -0,0 +1,31 @@
+{ # try
+
+    bunzip2 "vid.bz2"
+
+} || { # catch
+    echo ""
+}
+
+
+python <(cat <<EOF
+import sys
+import time
+fps = 25; time_per_frame = 1.0 / fps
+buf = ''; frame = 0; next_frame = 0
+begin = time.time()
+try:
+  for i, line in enumerate(sys.stdin):
+    if i % 32 == 0:
+      frame += 1
+      sys.stdout.write(buf); buf = ''
+      elapsed = time.time() - begin
+      repose = (frame * time_per_frame) - elapsed
+      if repose > 0.0:
+        time.sleep(repose)
+      next_frame = elapsed / time_per_frame
+    if frame >= next_frame:
+      buf += line
+except KeyboardInterrupt:
+  pass
+EOF
+) < "vid"
